b56829a614373242aafca924349db61482d44107
양다민 <yangdamin@yangdamin-ui-MacBookAir.local>
2022-04-18 01:15:07 +0900

    진행 중

---
+++ demo/src/main/java/com/example/demo/model/Map.java
@@ -0,0 +1,21 @@
+// package com.example.demo.model;
+
+// import javax.persistence.Entity;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.Id;
+
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
+
+// @Entity
+// @Data
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class Map {
+//     @Id
+//     @GeneratedValue
+//     private long id;
+
+//     private
+// }
